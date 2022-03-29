16567bddd713d191a4e525ab7d3b2367e4d34896
Emre Tuğ <emree68.12@gmail.com>
2022-03-29 18:32:17 +0300

    - emretug

---
+++ PratikKargo/PratikKargo/PratikKargo/Bee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PratikKargo
+{
+    class Bee
+    {
+        public int status;
+        public Point[] memoryPath;
+        public double measureOfQuality;
+        public int numberOfVisits;
+
+        public Bee(int stat, Point[] path, double measure, int numVisits)
+        {
+            this.status = stat;
+            this.memoryPath = path;
+            this.measureOfQuality = measure;
+            this.numberOfVisits = numVisits;
+        }
+    }
+}
---
+++ PratikKargo/PratikKargo/PratikKargo/Model/DistanceModel.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PratikKargo.Model
+{
+   public  class DistanceModel
+    {
+        public partial class DistanceResponseModel
+        {
+            [JsonProperty("destination_addresses")]
+            public string[] DestinationAddresses { get; set; }
+
+            [JsonProperty("origin_addresses")]
+            public string[] OriginAddresses { get; set; }
+
+            [JsonProperty("rows")]
+            public Row[] Rows { get; set; }
+
+            [JsonProperty("status")]
+            public string Status { get; set; }
+        }
+
+        public partial class Row
+        {
+            [JsonProperty("elements")]
+            public Element[] Elements { get; set; }
+        }
+
+        public partial class Element
+        {
+            [JsonProperty("distance")]
+            public Distance Distance { get; set; }
+
+            [JsonProperty("duration")]
+            public Distance Duration { get; set; }
+
+            [JsonProperty("status")]
+            public string Status { get; set; }
+        }
+
+        public partial class Distance
+        {
+            [JsonProperty("text")]
+            public string Text { get; set; }
+
+            [JsonProperty("value")]
+            public long Value { get; set; }
+        }
+    }
+}
