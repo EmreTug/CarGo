f560c3abe0089918a13ced8573f407e38a0e3263
Emre Tuğ <emree68.12@gmail.com>
2022-03-31 14:50:21 +0300

    -

--- PratikKargo/PratikKargo/PratikKargo.Android/obj/Debug/110/android/environment.x86.s
+++ PratikKargo/PratikKargo/PratikKargo.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"0c7c3b6f-dbbd-4f7a-b5a5-b8ca38b46563"
+	.asciz	"db3443d8-e5be-4a14-8cf0-e6368d92164c"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
