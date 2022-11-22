7c54bf08d2972c4396f5ecb87d33a78297c1dc47
Emre Tuğ <92733367+EmreTug@users.noreply.github.com>
2022-11-22 17:04:07 +0300

    Update AppConstants.cs

--- PratikKargo/PratikKargo/PratikKargo/Constants/AppConstants.cs
+++ PratikKargo/PratikKargo/PratikKargo/Constants/AppConstants.cs
@@ -6,6 +6,6 @@ namespace PratikKargo.Constants
 {
     public class AppConstants
     {
-        public const string GoogleMapsApiKey = "AIzaSyAAriP1Z3O9jQrsXAEaTby5e0XXEs2Sq8I";
+        public const string GoogleMapsApiKey = "YOUR API KEY";
     }
 }
